2e3dfbe0446f0c8777a9ad5d01cbd7ab45901250
WEBUPDRNEW_V1 <leandrofrancov48@gmail.com>
2026-04-09 14:55:29 -0300

    tune: subir encuadre hero de forma mas visible

--- components/HeroCover.tsx
+++ components/HeroCover.tsx
@@ -27,7 +27,7 @@ export default function HeroCover() {
           src="/hero-updr.png"
           alt="Un Poco De Ruido"
           fill
-          className="object-cover object-[center_16%] md:object-[center_22%]"
+          className="object-cover object-[center_12%] md:object-[center_18%]"
           priority
         />
 
